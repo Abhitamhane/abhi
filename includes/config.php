f3fc415368182b6f9d71a475f2481c5b31cd6ef6
Abhitamhane <atamhane74@gmail.com>
2021-05-05 18:36:49 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,18 @@
 <?php 
-// DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','abhi');
-// Establish database connection.
+
+
+define('DB_HOST','remotemysql.com');
+define('DB_USER','TwvAbM8z3z');
+define('DB_PASS','tIJq2t2Iov');
+define('DB_NAME','TwvAbM8z3z')
+
+
+// // DB credentials.
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASS','');
+// define('DB_NAME','abhi');
+// // Establish database connection.
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
